85e87cd7389c821c024b2a1574cbf14ff876bb04
Miko Dagatan <miguel.dagatan@gmail.com>
2025-04-05 00:37:37 +0800

    Create user through Devise

---
+++ config/initializers/frozen_string_literal.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Enable frozen string literals for all Ruby files loaded after this point
+Ruby::Language.use_frozen_string_literal = true if defined?(Ruby::Language)
