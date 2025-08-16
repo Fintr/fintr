ee9c6928b4010593fa56dff4d652972ba20e2fe0
Miko Dagatan <miguel.dagatan@gmail.com>
2025-08-16 18:57:56 +0800

    Change calculate pending balances job to use date range

--- app/jobs/transactions/transfers/calculate_pending_balances_job.rb
+++ app/jobs/transactions/transfers/calculate_pending_balances_job.rb
@@ -5,8 +5,9 @@ module Transactions
     class CalculatePendingBalancesJob < ApplicationJob
       queue_as :default
 
-      def perform
-        query = Transactions::Transfer.where(balance_state: "pending", date: Time.zone.today)
+      def perform(date: Date.current)
+        parsed_date = Date.parse(date) if date.is_a?(String)
+        query = Transactions::Transfer.where(balance_state: "pending", date: parsed_date.beginning_of_day..parsed_date.end_of_day)
 
         query.find_each(batch_size: 100) do |transfer|
           params = { transfer_id: transfer.id }
