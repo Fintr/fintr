623ae0ec3d466ef76a42be4acab15943f8045163
Miko Dagatan <miguel.dagatan@gmail.com>
2025-09-01 13:30:48 +0800

    Update code for time zone usage

--- app/jobs/transactions/transfers/check_duplicate_today_job.rb
+++ app/jobs/transactions/transfers/check_duplicate_today_job.rb
@@ -13,10 +13,11 @@ module Transactions
         Rails.logger.info("Found #{query.count} transfers with schedule")
 
         query.find_each(batch_size: 100) do |transfer|
+          date = Time.zone.today.date.in_time_zone("Asia/Manila")
           schedule = IceCube::Schedule.from_hash(transfer.schedule)
           occurs = schedule.occurring_between?(
-            Time.zone.today.in_time_zone("Asia/Manila").at_beginning_of_day,
-            Time.zone.today.in_time_zone("Asia/Manila").at_end_of_day
+            date.at_beginning_of_day,
+            date.at_end_of_day
           )
           next unless occurs
 
