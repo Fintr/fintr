0bb93340b665fe98121cf29290b07063de53f0b0
Miko Dagatan <miguel.dagatan@gmail.com>
2026-03-26 15:11:34 +0800

    [Bugfix] Reapply fixes

--- src/plugins/navigation-info.ts
+++ src/plugins/navigation-info.ts
@@ -1,5 +1,3 @@
-import { registerPlugin } from '@capacitor/core';
-
 export interface NavigationInfo {
   navMode: number;
   is3ButtonNavigation: boolean;
@@ -12,16 +10,30 @@ export interface NavigationInfoPlugin {
   applySafeAreaClasses(): Promise<void>;
 }
 
-export const NavigationInfo = registerPlugin<NavigationInfoPlugin>('NavigationInfo', {
-  web: () => Promise.resolve({
-    getNavigationInfo: async () => ({
-      navMode: -1,
-      is3ButtonNavigation: false,
-      isGestureNavigation: false,
-      platform: 'web'
-    }),
-    applySafeAreaClasses: async () => {
-      console.log('[NavigationInfo] Web shim - no native classes to apply');
-    }
-  } as NavigationInfoPlugin)
-});
+const webFallback: NavigationInfoPlugin = {
+  getNavigationInfo: async () => ({
+    navMode: -1,
+    is3ButtonNavigation: false,
+    isGestureNavigation: false,
+    platform: 'web'
+  }),
+  applySafeAreaClasses: async () => {
+    console.log('[NavigationInfo] Web shim - no native classes to apply');
+  }
+};
+
+let NavigationInfoExport: NavigationInfoPlugin;
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const capacitorCore = require('@capacitor/core');
+  const registerPlugin = capacitorCore.registerPlugin as <T>(name: string, opts?: object) => T;
+  NavigationInfoExport = registerPlugin<NavigationInfoPlugin>('NavigationInfo', {
+    web: () => Promise.resolve(webFallback)
+  });
+} catch (error) {
+  console.warn('[NavigationInfo] Failed to register plugin:', error);
+  NavigationInfoExport = webFallback;
+}
+
+export { NavigationInfoExport as NavigationInfo };
